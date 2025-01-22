0e2c30e1e76ac486cad5d50f5472c60024e3a4e9
Ortello7 <domenicoortello00@gmail.com>
2025-01-22 22:20:57 +0100

    feat controller functions

--- routers/posts.js
+++ routers/posts.js
@@ -1,30 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const postsDataElm = require("../postsData")
+
+// Importo funzioni controller
+const controller = require("../controllers/postsControllers");
 
 // index
-router.get('/', function (req, res) {
-    res.json('Lista dei post');
-});
+router.get('/', controller.index);
+
 // show
-router.get('/:id', function (req, res) {
-    res.json(`Dettagli del post: ${req.params.id}`);
-});
-// store
-router.post('/', function (req, res) {
-    res.json('Creazione nuovo post');
-});
-// update
-router.put('/:id', function (req, res) {
-    res.json(`Modifica integrale del post: ${req.params.id}`);
-});
+router.get('/:id', controller.show);
+
+// Create
+router.post('/', controller.create);
+
+// Update
+router.put('/:id', controller.update);
+
 // modify
-router.patch('/:id', function (req, res) {
-    res.json(`Modifica parziale del post: ${req.params.id}`);
-});
+router.patch('/:id', controller.update);
+
+// Delete
+router.patch('/:id', controller.update);
+
 // destroy
-router.delete('/:id', function (req, res) {
-    res.json(`Eliminazione del post: ${req.params.id}`);
-});
+router.delete('/:id', controller.destroy);
 
 module.exports = router;
