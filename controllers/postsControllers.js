bee80f13418c4b07df469ccf2cc9a9d90df8e6bc
Ortello7 <domenicoortello00@gmail.com>
2025-01-23 16:54:59 +0100

    feat middleware

--- controllers/postsControllers.js
+++ controllers/postsControllers.js
@@ -1,55 +1,68 @@
-const postsData = require ('./postsData');
+// IMPORTO L'ARRAY DEI POST
+const postsData = require("../data/allPosts")
 
 // INDEX
 const index = function (req, res) {
-    res.json(`Lista dei post`); 
+    res.json(postsData);
 };
+
 // SHOW
-const show = function (req, res) {
+const show = function (req, res,) {
     if(isNaN(req.params.id)){
         return res.sendStatus(400)
     }
+
     const post = postsData.find((elm) => elm.id == req.params.id)
+
     if (!post){
        res.sendStatus(404)
     }   
     res.json(`Dettagli di un post ${req.params.id}`);
 }
+
 // CREATE
 const create = function (req, res) {
     res.json(`Creazione nuovo post`);
 }
+
 // UPDATE
 const update = function (req, res) {
     if(isNaN(req.params.id)){
         return res.sendStatus(400)
     }
+
     const post = postsData.find((elm) => elm.id == req.params.id)
+
     if (!post){
        res.sendStatus(404)
     } 
     res.json(`Modifica integrale del post ${req.params.id} `);
 }
+
 // MODIFY
 const modify = function (req, res) {
     if(isNaN(req.params.id)){
         return res.sendStatus(400)
     }
+
     const post = postsData.find((elm) => elm.id == req.params.id)
+
     if (!post){
        res.sendStatus(404)
     } 
     res.json(`Modifica parziale del post ${req.params.id} `);
 }
+
 // DESTROY
 const destroy = function (req, res) {
     if(isNaN(req.params.id)){
         return res.sendStatus(400)
     }
+
     const post = postsData.find((elm) => elm.id == req.params.id)
+
     if (!post){
        res.sendStatus(404)
     } 
     res.json(`Eliminazione del post ${req.params.id} `);
 }
-module.exports = {index, show, create, update, modify, destroy}
